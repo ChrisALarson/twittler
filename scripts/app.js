78e5338ac7602180b1efad633c0902d07daec748
Chris Larson <larson07@gmail.com>
2018-07-12 21:46:53 -0700

    refactor JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,101 +1,114 @@
 $(document).ready(function(){
+  /* 
+  ------------------------------
+  SETUP
+  ------------------------------
+  */
+  
   //jquery objects
   let $body = $('body');
-  let $trendsDisplay = $('.trends-display');
-  let $tweetForm = $('.tweet-form');
 
   // state variables
   let targetUser = 'all';
   let targetTrend = '';
 
-  // filters
-  const filterUser = (userName) => {
+
+  /* 
+  ------------------------------
+  ACTIONS
+  ------------------------------
+  */
+
+  function filterUser(userName) {
     targetUser = userName;
     targetTrend = '';
     renderDisplay();
-  };
+  }
 
-  const filterTrend = (trend) => {
+  function filterTrend(trend) {
     targetTrend = trend;
     targetUser = '';
     renderDisplay();
-  };
+  }
 
-  // like and retweet
-  const addLike = (tweetIndex, tweetTextContent = '') => {
-    streams.home[tweetIndex].likes = streams.home[tweetIndex].likes + 1;
-    let newLikeCount = streams.home[tweetIndex].likes;
-    console.log(`Liked "${tweetTextContent}"! Total likes: `, newLikeCount);
+  function addLike(tweetIndex) {
+    let newLikeCount = ++streams.home[tweetIndex].likes;
     return newLikeCount;
-  };
+  }
 
-  const addRetweet = (tweetIndex, tweetTextContent = '', tweetUser = '') => {
-    streams.home[tweetIndex].retweets = streams.home[tweetIndex].retweets + 1;
-    let newRetweetCount = streams.home[tweetIndex].retweets;
+  function addRetweet(tweetIndex, tweetTextContent, tweetUser) {
+    let newRetweetCount = ++streams.home[tweetIndex].retweets;
     let message = `RT: @${tweetUser}: ${tweetTextContent}`;
     setTimeout(writeTweet, 150, message);
     setTimeout(renderDisplay, 150);
     return newRetweetCount;
-  };
+  }
 
-  const addComment = (tweetIndex, tweetTextContent = '', comment) => {
+  function addComment(tweetIndex, comment) {
     let newCommentCount = streams.home[tweetIndex].comments.push(comment);
     return newCommentCount;
   }
 
-  // event handlers
-  $trendsDisplay.on('click', '.trend', function() {
+
+  /* 
+  ------------------------------
+  EVENT HANDLERS
+  ------------------------------
+  */
+
+  // viewing trend's timeline
+  $body.on('click', '.trend', function() {
     let trend = $(this).context.dataset.trend;
     filterTrend(trend);
   });
 
+  // viewing user's timeline 
   $body.on('click', '.user-link', function() {
-    let user = $(this).context.dataset.user;
+    let user = $(this).context.dataset.user || $(this).data('user');
     filterUser(user);
   });
 
+  // liking
   $body.on('click', '.like', function() {
-    let tweetIndex = $(this).context.parentElement.parentElement.parentElement.dataset.homeindex;
-    let tweetText = $(this).context.parentElement.parentElement.parentElement.children[1].children[1].innerText;
+    let tweetIndex = $(this).data('index');
+    let tweetText = $(this).data('message');
     let newLikeCount = addLike(tweetIndex, tweetText);
-    $(this).context.nextSibling.innerHTML = newLikeCount.toString();
+    $(this).next().html(newLikeCount.toString());
     $(this).attr('src', 'images/like-filled.png');
   });
 
+  // retweeting
   $body.on('click', '.retweet', function() {
-    let tweetIndex = $(this).context.parentElement.parentElement.parentElement.dataset.homeindex;
-    let tweetText = $(this).context.parentElement.parentElement.parentElement.children[1].children[1].innerText;
-    let tweetUser = $(this).context.parentElement.parentElement.parentElement.children[1].children[0].dataset.user;
+    let tweetIndex = $(this).data('index');
+    let tweetText = $(this).data('message');
+    let tweetUser = $(this).data('user');
     let newRetweetCount = addRetweet(tweetIndex, tweetText, tweetUser);
-    $(this).context.nextSibling.innerHTML = newRetweetCount.toString();
+    $(this).next().html(newRetweetCount.toString());
     $(this).attr('src', 'images/retweet-filled.png');
   });
 
+  // commenting
   $body.on('click', '.comment', function() {
-    let tweetIndex = $(this).context.parentElement.parentElement.parentElement.dataset.homeindex;
-    let tweetText = $(this).context.parentElement.parentElement.parentElement.children[1].children[1].innerText;
+    let tweetIndex = $(this).data('index');
     let commentText = prompt('What do you think about this tweet?');
-    let comment = {
-      commenter: '@visitor',
-      comment: commentText
-    };
-    let newCommentCount = addComment(tweetIndex, tweetText, comment);
-    $(this).context.nextSibling.innerHTML = newCommentCount.toString();
+    let comment = { commenter: '@visitor', comment: commentText };
+    let newCommentCount = addComment(tweetIndex, comment);
+    $(this).next().html(newCommentCount.toString());
     $(this).attr('src', 'images/comment-filled.png');
   });
 
-  $body.on('click', '.comments-display', function() {
-    let tweetIndex = $(this).context.parentElement.parentElement.parentElement.dataset.homeindex;
+  // viewing comments
+  $body.on('click', '.comment-display', function() {
+    let tweetIndex = $(this).data('index');
     let comments = streams.home[tweetIndex].comments;
-    let commentsDisplay = '';
-    comments.forEach(comment => {
-      commentsDisplay += `${comment.commenter}: ${comment.comment}`
-      commentsDisplay += '\n';
-    });
-    alert(commentsDisplay);
+    let commentsString = comments.reduce((total, current) => {
+      return total + `${current.commenter}: ${current.comment}` + '\n';
+    }, '');
+    alert(commentsString);
   });
 
-  $tweetForm.on('submit', function(event) {
+  // posting tweet
+  $body.on('submit', '.tweet-form', function(event) {
     event.preventDefault();
     let $tweetTextbox = $('.tweet-textbox');
     if ($tweetTextbox.val()) {
@@ -105,62 +118,144 @@ $(document).ready(function(){
     $tweetTextbox.val('');
   });
 
-  // renderers
-  const renderFollowers = () => {
-    let $followers = $('.followers');
-    $followers.html('');
 
-    users.forEach((user) => {
-      let $follower = $(`<p class="follower user-link" data-user="${user}">@${user}</p>`);
-      $followers.append($follower);
-    });
-   };
+  /* 
+  ------------------------------
+  RENDERERS
+  ------------------------------
+  */
 
-  const renderTrends = () => {
-    let trends = Object.keys(streams.trends);
-    trends.sort((a,b) => {
-      if (streams.trends[a].length >= streams.trends[b].length) {
-        return -1;
-      } else if (streams.trends[a].length < streams.trends[b].length) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-
+ // followers aside
+ function renderFollowers() {
+   let $followers = $('.followers');
+   $followers.html('');
+   users.forEach((user) => {
+     let $follower = $(`<p class="follower user-link" data-user="${user}">@${user}</p>`);
+     $followers.append($follower);
+   });
+ }
+ 
+ // tweets display
+ function renderTweets() {
+   let $tweetsDisplay = $('.tweets-display');
+    $tweetsDisplay.html('');
+    let tweets = getTargetTweets();
+    let index = tweets.length - 1;
+    for (let i = index; i >= 0; i--) {
+      let tweet = tweets[i];
+      let $tweet = createTweetContainer(tweet);
+      $tweetsDisplay.append($tweet);
+    }
+  }
+  
+  // trends aside
+  function renderTrends() {
     let $trends = $('.trends');
     $trends.html('');
-    
+    let trends = Object.keys(streams.trends);
+    trends.sort((trendOne, trendTwo) => {
+      let numTrendOne = streams.trends[trendOne].length;
+      let numTrendTwo = streams.trends[trendTwo].length;
+      return (numTrendOne >= numTrendTwo) ? -1 : 1;
+    });
     trends.forEach((trend) => {
       let $trend = $(`<h3 class="trend" data-trend="${trend}">${trend}</h3>`);
       if (trend) $trends.append($trend);
     });
-   };
+  }
 
-  const renderProfile = () => {
-    let $profileData = $('.profile-data');
-    $profileData.html('');
-    let $profileImageContainer = $('.profile-img-container');
-    $profileImageContainer.html('');
-
+  // profile aside
+  function renderProfile() {
     let user = (targetUser === 'all' || targetTrend) ? 'visitor' : targetUser;
     let numUserTweets = streams.users[user].length;
-    // generating number of followers based on current date (for us) or user name (for prebuilt users)
-    let numUserFollowers = (user === 'visitor') ? Math.floor((new Date() - 1499999999999) / 10000) : user.split('').reduce((acc, element) => acc + element.charCodeAt(), 0) * 2;    
+    let numUserFollowers = makeFollowerCount(user);    
 
-    let $userProfileImg = $(`<img src="images/${user}.png" alt="Profile Picture" class="profile-img user-link" data-user="${user}"></img>`)
+    // clear existing profile
+    let $profile = $('.profile');
+    $profile.html('');
+
+    // user's image
+    let $profileImageContainer = $(`<div class="profile-img-container"></div>`);
+    let $userProfileImg = $(`<img src="images/${user}.png" alt="Profile Picture" class="profile-img user-link"></img>`)
     $profileImageContainer.append($userProfileImg);
-
-    let $userHandle = $(`<h2 class="user-handle user-link" data-user="${user}">@${user}</h2>`);
-    let $tweetCount = $(`<p class="tweet-count user-link" data-user="${user}">${numUserTweets} tweets</p>`);
+    
+    // user's data
+    let $profileData = $(`<div class="profile-data"></div>`);
+    let $userHandle = $(`<h2 class="user-handle user-link>@${user}</h2>`);
+    let $tweetCount = $(`<p class="tweet-count user-link">${numUserTweets} tweets</p>`);
     let $followerCount = $(`<p class="follower-count">${numUserFollowers.toLocaleString()} followers</p>`);
     $profileData.append($userHandle, $tweetCount, $followerCount);
+
+    //add metadata to profile elements
+    $profile.append($profileImageContainer, $profileData);
+    $profile.find('*').data('user', user);
+  }
+
+  function renderDisplay() {
+    renderFollowers();
+    renderTweets();
+    renderProfile();
+    renderTrends();
+  }
+
+
+  /* 
+  ------------------------------
+  UTILITY FUNCTIONS
+  ------------------------------
+  */
+
+  function createTweetContainer(tweet) {
+    // tweeter's avatar
+    let $avatar = $(`<div class="avatar-img-container"><img src="images/${tweet.user}.png" class="avatar-img user-link"/></div>`);
+
+    // main tweet content
+    let $content = $(`<div class="content"></div>`);
+    let $user = $(`<p class="handle user-link">@${tweet.user}</p>`);
+    let $message = $(`<p class="message">${tweet.message}</p>`);
+    let $createdAt = $(`<p class="time">${moment(tweet.created_at).fromNow()}</p>`);
+    $content.append($user, $message, $createdAt);
+
+    // tweet actions and metadata
+    let $actions = $(`<div class="tweet-actions"></div>`);
+    let $like = createActionContainer('like', (tweet.likes));
+    let $retweet = createActionContainer('retweet', (tweet.retweets));
+    let $comment = createActionContainer('comment', (tweet.comments.length));
+    $actions.append($like, $retweet, $comment);
+
+    // tweet body
+    let $tweet = $(`<div class="tweet"></div>`);
+    $tweet.append($avatar, $content, $actions);
+
+    // add data attributes to all elements in tweet
+    $tweet.find('*').data('user', tweet.user);
+    $tweet.find('*').data('message', tweet.message);
+    $tweet.find('*').data('index', tweet.homeIndex);
+
+    return $tweet;
+
+    // utility function
+    function createActionContainer(name, prop) {
+      let img = (prop) ? `${name}-filled` : `${name}`;
+      let $container = $(`<div class="${name}-container tweet-data"></div>`);
+      let $button = $(`<img src="images/${img}.png" alt="${name}" class="tweet-action ${name}"></img>`);
+      let $numData = $(`<span class="${name}-display">${prop}</span>`);
+      $container.append($button, $numData);
+      return $container;
+    } 
   };
 
-  const renderTweets = () => {
-    let $tweetsDisplay = $('.tweets-display');
-    $tweetsDisplay.html('');
-    
+  function makeFollowerCount(user) {
+    let followerCount;
+    if (user === 'visitor') {
+      followerCount = Math.floor((new Date() - 1499999999999) / 10000);
+    } else {
+      followerCount = user.split('').reduce((acc, char) => acc + char.charCodeAt(), 0) * 2;
+    }
+    return followerCount;
+  }
+
+  function getTargetTweets() {
     let tweets;
     if (targetUser === 'all') {
       tweets = streams.home;
@@ -168,53 +263,18 @@ $(document).ready(function(){
       tweets = streams.users[targetUser];
     } else if (targetTrend) {
       tweets = streams.trends[targetTrend];
+    } else {
+      return streams.home;
     }
+    return tweets;
+  }
 
-    let index = tweets.length - 1;
-    for (let i = index; i >= 0; i--) {
-      let tweet = tweets[i];
 
-      let $tweet = $(`<div class="tweet" data-homeindex="${tweet.homeIndex}"></div>`);
-      let $avatar = $(`<div class="avatar-img-container"><img src="images/${tweet.user}.png" class="avatar-img user-link" data-user="${tweet.user}"/></div>`);
-      let $content = $(`<div class="content"></div>`);
-      let $buttons = $(`<div class="buttons"></div>`);
-
-      let $user = $(`<p class="handle user-link" data-user="${tweet.user}">@${tweet.user}</p>`);
-      let $message = $(`<p class="message">${tweet.message}</p>`);
-      let $created_at = $(`<p class="time">${moment(tweet.created_at).fromNow()}</p>`);
-      $content.append($user, $message, $created_at);
-  
-      let retweetImg = (tweet.retweets) ? 'retweet-filled' : 'retweet';
-      let $retweetContainer = $(`<div class="retweets-container tweet-data"></div>`);
-      let $retweetButton = $(`<img src="images/${retweetImg}.png" alt="Retweet" class="tweet-action retweet"></img>`);
-      let $numRetweets = $(`<span class="retweets-display">${tweet.retweets}</span>`);
-      $retweetContainer.append($retweetButton, $numRetweets);
-
-      let likeImg = (tweet.likes) ? 'like-filled' : 'like';
-      let $likeContainer = $(`<div class="likes-container tweet-data"></div>`);
-      let $likeButton = $(`<img src="images/${likeImg}.png" alt="Like" class="tweet-action like"></img>`);
-      let $numLikes = $(`<span class="likes-display">${tweet.likes}</span>`);
-      $likeContainer.append($likeButton, $numLikes);
-
-      let commentImg = (tweet.comments.length) ? 'comment-filled' : 'comment';
-      let $commentContainer = $(`<div class="comments-container tweet-data"></div>`);
-      let $commentButton = $(`<img src="images/${commentImg}.png" alt="Comment" class="tweet-action comment"></img>`)
-      let $numComments = $(`<span class="comments-display">${tweet.comments.length}</span>`);
-      $commentContainer.append($commentButton, $numComments);
-
-      $buttons.append($likeContainer, $retweetContainer, $commentContainer);
-
-      $tweet.append($avatar, $content, $buttons);
-      $tweetsDisplay.append($tweet);
-    }
-   };
-
-  const renderDisplay = () => {
-    renderFollowers();
-    renderTweets();
-    renderProfile();
-    renderTrends();
-  };
+  /* 
+  ------------------------------
+  LAUNCH APP
+  ------------------------------
+  */
 
   renderDisplay();
   setInterval(renderDisplay, 20000);
